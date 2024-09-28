540f3d1df5df189e21ff481b9a975dc7dc525fd4
Luca Barreca <lbarreca@e3r2p3.42roma.it>
2024-09-28 19:05:59 +0200

    mariano

---
+++ rush01/ex00/main.c
@@ -0,0 +1,36 @@
+int chkcharh(char *linea)
+{
+	int	
+}
+
+char *extrval(char *str)
+{
+	int	i;
+	int	j;
+	char strex[16];
+
+	i=0;
+	j=0;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <='9')
+		{
+			strex[j];
+			j++
+		}
+		i++
+	}
+}
+
+
+
+int main(int ac, char **av)
+{
+	char	v[16];
+	
+	if (ac != 2)
+		return 0;
+	*v = extrval(av[1])
+
+
+}
