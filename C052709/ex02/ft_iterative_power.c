b2a04007e1733afcee41a8b89267ac665c31ff30
hypno197 <luca_barreca@hotmail.it>
2024-10-01 20:52:11 +0200

    si

--- C052709/ex02/ft_iterative_power.c
+++ C052709/ex02/ft_iterative_power.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbarreca <lbarreca@student.roma42.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 20:22:07 by lbarreca          #+#    #+#             */
+/*   Updated: 2024/10/01 20:51:25 by lbarreca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+//#include <stdio.h>
+//#include <stdlib.h>
 int ft_iterative_power(int nb, int power)
 {	
 	int	res;
@@ -15,9 +26,9 @@ int ft_iterative_power(int nb, int power)
 		return 1;
 	return res;
 }
-
+/*
 int main(int ac, char **av)
 {
         printf("%d\n", ft_iterative_power(atoi(av[1]), atoi(av[2])));
         return 0;
-}
+}*/
